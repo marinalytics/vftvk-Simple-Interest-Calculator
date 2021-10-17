85d99a5d4198f967bc95c655fdbc0290709c903a
marinalytics <vivek@marinalytics.com>
2021-10-17 08:53:04 +0000

    changes made for hands-on-project

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * rate * years / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    if(principal<=0) {
+        alert("Enter a positive number")
+        document.getElementById('principal').focus()
+    } else {
+        document.getElementById("result").innerText="If you deposit "+principal+",\n at an interest rate of "+rate+"%,\n You will receive an amount of "+interest+",\n in the year "+year+"\n"
+    }
 }
         
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+'%';
+}
