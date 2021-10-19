7ee324f14a09b001d5611e0466815daa25d5d26f
marinalytics <87240877+marinalytics@users.noreply.github.com>
2021-10-19 14:38:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,24 @@ function compute()
         alert("Enter a positive number")
         document.getElementById('principal').focus()
     } else {
-        document.getElementById("result").innerText="If you deposit "+"<span class='highlight'>"+principal+"</span>"+",\n at an interest rate of "+"<span class='highlight'>"+rate+"</span>"+"%,\n You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+",\n in the year "+"<span class='highlight'>"+year+"</span>"+"\n"
+        var resultDisplay = document.getElementById("result");
+        resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
     }
+}
+
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}
+
 }
         
 function updateRate()
