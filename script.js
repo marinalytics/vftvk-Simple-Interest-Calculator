629f10ccedd60f27801c59d8d96c6f4082961235
marinalytics <87240877+marinalytics@users.noreply.github.com>
2021-10-19 14:49:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,8 @@ function compute()
     if(principal<=0 || principal=='') {
         alert("Enter a positive number")
         document.getElementById('principal').focus()
-    } else {
-        var resultDisplay = document.getElementById("result");
-        resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
-    }
+    } 
+    document.getElementById("result").innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + year + "</span>";
 }
 
 function SliderValue()
